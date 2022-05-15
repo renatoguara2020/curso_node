6badb974c9412a8bd7d95c67a9331f24b15458c7
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-15 09:31:47 -0300

    commit changes

--- 13_MONGOOSE/3_inserindo_dados/db/conn.js
+++ 13_MONGOOSE/3_inserindo_dados/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/testemongoose')
+  await mongoose.connect('mongodb://localhost:27017/mongo_mongoose2')
   console.log('Conectou com Mongoose!')
 }
 
